05dcea05ba3df0d85a9391346fe9bb96c4826d4c
TreeSap <jgwiggenhauser@gmail.com>
2019-04-30 23:12:53 -0300

    Commit inicial

---
+++ AppSencilla/src/com/JuanWigg/Main.java
@@ -0,0 +1,14 @@
+
+package com.JuanWigg;
+
+
+public class Main {
+    
+    public static void main(String [] args){
+        System.out.println("Hola Mundo");
+        
+        
+    }
+    
+    
+}
