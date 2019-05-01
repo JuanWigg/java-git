65d9d8b8bffce37216e7d1a43137d3a2c92b3be5
TreeSap <jgwiggenhauser@gmail.com>
2019-05-01 15:05:32 -0300

    Se ubicaron los componentes en VentanaRegis.

--- AppSencilla/src/com/JuanWigg/VentanaRegis.java
+++ AppSencilla/src/com/JuanWigg/VentanaRegis.java
@@ -8,13 +8,15 @@ import javax.swing.*;
 public class VentanaRegis extends JFrame {
     JButton botonCancel;
     JButton botonRegis;
-    JButton label_user;
-    JButton label_pass;
-    JButton label_email;
-    JButton label_fechaNac;
+    JLabel label_user;
+    JLabel label_pass;
+    JLabel label_email;
+    JLabel label_fechaNac;
     JPanel panel;
-    
-    
+    datechooser.beans.DateChooserCombo fecha = new datechooser.beans.DateChooserCombo();
+    JTextField text_user;
+    JTextField text_email;
+    JPasswordField text_pass;
     
     public VentanaRegis(){
         setVisible(true);
@@ -22,6 +24,7 @@ public class VentanaRegis extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("WiggenSoft");
+        setResizable(false);
         
         panel = new JPanel();
         panel.setLayout(null);
@@ -37,18 +40,60 @@ public class VentanaRegis extends JFrame {
     
     
     private void configLabels(){
-        //XD
+        
+        label_user = new JLabel("Nombre de usuario:");
+        label_pass = new JLabel("Contraseña:");
+        label_email = new JLabel("E-mail:");
+        label_fechaNac = new JLabel("Fecha de nacimiento:");
+        
+        label_user.setBounds(200,100,200,30);
+;       label_pass.setBounds(200,200,200,30);
+        label_email.setBounds(200,300,200,30);
+        label_fechaNac.setBounds(200,400,200,30);
+        
+        label_user.setForeground(Color.MAGENTA);
+        label_pass.setForeground(Color.MAGENTA);
+        label_email.setForeground(Color.MAGENTA);
+        label_fechaNac.setForeground(Color.MAGENTA);
+        
+        
+        panel.add(label_user);
+        panel.add(label_pass);
+        panel.add(label_email);
+        panel.add(label_fechaNac);
+        
         
     }
     
     private void configTextFields(){
         
+        text_user = new JTextField();
+        text_email = new JTextField();
+        text_pass = new JPasswordField();
+        
+        text_user.setBounds(200,130,200,30);
+        text_email.setBounds(200,330,200,30);
+        text_pass.setBounds(200,230,200,30);
+        
+        panel.add(text_user);
+        panel.add(text_pass);
+        panel.add(text_email);
+        
+        fecha.setBounds(200,430,200,30);
+        panel.add(fecha);
         
     }
     
     private void configBotones(){
+        botonRegis = new JButton("Registrarse");
+        botonCancel = new JButton("Cancelar");
+        
+        botonRegis.setBounds(300,550,200,100);
+        botonCancel.setBounds(75,550,200,100);
         
         
+        panel.add(botonRegis);
+        panel.add(botonCancel);
     }
     
 }
