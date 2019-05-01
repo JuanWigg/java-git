b452d85b154c1291f4f5f6ba9c4f3ad52863e949
TreeSap <jgwiggenhauser@gmail.com>
2019-05-01 14:22:31 -0300

    Se creó la clase VentanaRegis.

---
+++ AppSencilla/src/com/JuanWigg/VentanaRegis.java
@@ -0,0 +1,54 @@
+
+package com.JuanWigg;
+
+import java.awt.Color;
+import javax.swing.*;
+
+
+public class VentanaRegis extends JFrame {
+    JButton botonCancel;
+    JButton botonRegis;
+    JButton label_user;
+    JButton label_pass;
+    JButton label_email;
+    JButton label_fechaNac;
+    JPanel panel;
+    
+    
+    
+    public VentanaRegis(){
+        setVisible(true);
+        setSize(600,800);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("WiggenSoft");
+        
+        panel = new JPanel();
+        panel.setLayout(null);
+        
+        getContentPane().add(panel);
+        configLabels();
+        
+        configTextFields();
+        configBotones();
+        
+        panel.setBackground(Color.blue);
+    }
+    
+    
+    private void configLabels(){
+        
+        
+    }
+    
+    private void configTextFields(){
+        
+        
+    }
+    
+    private void configBotones(){
+        
+        
+    }
+    
+}
